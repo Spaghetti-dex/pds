ed71e7286867d9a34eb0fca8fff852d962cce480
Yasmin <yasminpilapil1620@gmai.com>
2026-03-23 15:48:39 +0800

    for vercel deployment

--- api/index.php
+++ api/index.php
@@ -5,9 +5,9 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
-    header("Location: /pds/auth/login.php");
+    header("Location: ../auth/login.php");
     exit();
 }else {
-    header("Location: /pds/dashboard/dashboard.php");
+    header("Location: ../dashboard/dashboard.php");
 }
 ?>
