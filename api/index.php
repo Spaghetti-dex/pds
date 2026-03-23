4cea6fb6919313abba9078d67002b8861834a21f
Yasmin <yasminpilapil1620@gmai.com>
2026-03-23 16:41:45 +0800

    test4

--- api/index.php
+++ api/index.php
@@ -1,6 +1,14 @@
 <?php
- 
-    header("Location: /pds/auth/login.php"); 
-    exit();
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
+    header("Location: ../auth/login.php");
+    exit();
+}else {
+    header("Location: ../dashboard/dashboard.php");
+    exit();
+}
 ?>
--- includes/auth_check.php
+++ includes/auth_check.php
@@ -4,7 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
-    header("Location: /pds/auth/login.php");
+    header("Location: ../auth/login.php");
     exit();
 }
 ?>
